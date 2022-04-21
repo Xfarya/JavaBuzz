5bd3e0d446b30a3a360b0d9aad459a2cb57f453a
Xfarya <farya243@gmail.com>
2022-04-21 16:12:11 +0100

    refactored code to make it more compact, removed the boolean methods. Also added a test to check for invalid input

--- src/main/java/com/example/JavaBuzz.java
+++ src/main/java/com/example/JavaBuzz.java
@@ -6,27 +6,22 @@ public class JavaBuzz {
     public static void main(String[] args) {
     }
 
-    public static String returnString(int isConvertible) {
-        if (isJavaBuzz(isConvertible)) {
+    public static String returnString(int number) {
+        if (number < 0) {
+            return output = "Invalid Input";
+        }
+        if (isDivisibleBy(number, 15)) {
            output = "JavaBuzz";
-        } else if (isJava(isConvertible)) {
+        } else if (isDivisibleBy(number, 3)) {
            output = "Java";
+        } else if (isDivisibleBy(number, 5))  {
+           output = "Buzz";
+        } else {
+           output = number + "";
         }
         return output;
     }
 
-    public static boolean isJavaBuzz(int number) {
-        return isDivisibleBy(number, 15);
-    }
-
-    public static boolean isJava(int number) {
-        return isDivisibleBy(number, 3);
-    }
-
-    public static boolean isBuzz(int number) {
-        return isDivisibleBy(number, 5);
-    }
-    {}
     public static boolean isDivisibleBy(int number, int divisor) {
         return number % divisor == 0;
     }
--- src/test/java/com/example/JavaBuzzTest.java
+++ src/test/java/com/example/JavaBuzzTest.java
@@ -6,36 +6,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class JavaBuzzTest {
 
-   @Test
-   public void javaBuzz() {
-        assertTrue(JavaBuzz.isJavaBuzz(15));
-    }
-
-    @Test
-    public void notJavaBuzz() {
-        assertFalse(JavaBuzz.isJavaBuzz(16));
-    }
-
-    @Test
-    public void java() {
-        assertTrue(JavaBuzz.isJava(3));
-    }
-
-    @Test
-    public void notJava() {
-        assertFalse(JavaBuzz.isJava(4));
-    }
-
-    @Test
-    public void buzz() {
-        assertTrue(JavaBuzz.isBuzz(5));
-    }
-
-    @Test
-    public void notBuzz() {
-        assertFalse(JavaBuzz.isBuzz(6));
-    }
-
     @Test
     public void returnStringJavaBuzz() {
        assertEquals("JavaBuzz", JavaBuzz.returnString(15));
@@ -46,5 +16,29 @@ class JavaBuzzTest {
         assertEquals("Java", JavaBuzz.returnString(3));
     }
 
+    @Test
+    public void returnStringBuzz() {
+        assertEquals("Buzz", JavaBuzz.returnString(5));
+    }
+
+    @Test
+    public void returnIntegerIfNotJavaBuzz() {
+        assertEquals("1", JavaBuzz.returnString(1));
+    }
+
+    @Test
+    public void returnIntegerIfNotJavaBuzzExample2() {
+        assertEquals("14", JavaBuzz.returnString(14));
+    }
+
+    @Test
+    public void returnIntegerIfNotJavaBuzzExample3() {
+        assertEquals("94358792", JavaBuzz.returnString(94358792));
+    }
+
+    @Test
+    public void invalidInput() {
+        assertEquals("Invalid Input", JavaBuzz.returnString(-10));
+    }
 
 }
